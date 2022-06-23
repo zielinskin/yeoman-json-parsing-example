b57da3a98d016b301078dbe8413df8a3069796f1
zielinskin <zielinskin@outlook.com>
2022-06-23 14:22:53 -0500

    add in read from input json file

--- app/index.js
+++ app/index.js
@@ -1,18 +1,25 @@
 'use strict';
+
 const Generator = require('yeoman-generator');
+const fs = require('fs');
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.log('Initializing...');
   }
   start() {
+    let rawData = fs.readFileSync('inputs.json');
+
+    let data = JSON.parse(rawData);
+
     this.fs.copyTpl(
         this.templatePath('template.txt'),
         this.destinationPath(
           `results/template.txt`
         ),
         {
-          testVar: "hello"
+          testVar: data.testVar
         }
     );
   }
